baee419fe4679eaa70f574161fc4b883e5250a4a
Anar Kafkas <anarkafkas@gmail.com>
2025-08-01 21:07:34 +0300

    Reorder methods

--- sdks/swift-manual/Sources/ComplexFernTypes/Shape.swift
+++ sdks/swift-manual/Sources/ComplexFernTypes/Shape.swift
@@ -1,6 +1,45 @@
 import Foundation
 
 public enum Shape: Codable, Hashable, Sendable {
+  case triangle(ComplexFernTypes.Triangle)
+  case square(ComplexFernTypes.Square)
+  case circle(ComplexFernTypes.Circle)
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let type = try container.decode(String.self, forKey: .type)
+    switch type {
+    case "triangle":
+      self = .triangle(try ComplexFernTypes.Triangle(from: decoder))
+    case "square":
+      self = .square(try ComplexFernTypes.Square(from: decoder))
+    case "circle":
+      self = .circle(try ComplexFernTypes.Circle(from: decoder))
+    default:
+      throw DecodingError.dataCorrupted(
+        DecodingError.Context(
+          codingPath: decoder.codingPath,
+          debugDescription: "Unknown shape type: \(type)"
+        )
+      )
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    switch self {
+    case .triangle(let data):
+      try container.encode("triangle", forKey: .type)
+      try data.encode(to: encoder)
+    case .square(let data):
+      try container.encode("square", forKey: .type)
+      try data.encode(to: encoder)
+    case .circle(let data):
+      try container.encode("circle", forKey: .type)
+      try data.encode(to: encoder)
+    }
+  }
+
   public struct Triangle: Codable, Hashable, Sendable {
     public let type: String = "triangle"
     public let a: Double
@@ -41,46 +80,6 @@ public enum Shape: Codable, Hashable, Sendable {
 
     private enum CodingKeys: String, CodingKey {
       case type, radius
-    }
-  }
-
-  case triangle(ComplexFernTypes.Triangle)
-  case square(ComplexFernTypes.Square)
-  case circle(ComplexFernTypes.Circle)
-
-  public init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    let type = try container.decode(String.self, forKey: .type)
-    switch type {
-    case "triangle":
-      self = .triangle(try ComplexFernTypes.Triangle(from: decoder))
-    case "square":
-      self = .square(try ComplexFernTypes.Square(from: decoder))
-    case "circle":
-      self = .circle(try ComplexFernTypes.Circle(from: decoder))
-    default:
-      throw DecodingError.dataCorrupted(
-        DecodingError.Context(
-          codingPath: decoder.codingPath,
-          debugDescription: "Unknown shape type: \(type)"
-        )
-      )
-    }
-  }
-
-  public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-
-    switch self {
-    case .triangle(let data):
-      try container.encode("triangle", forKey: .type)
-      try data.encode(to: encoder)
-    case .square(let data):
-      try container.encode("square", forKey: .type)
-      try data.encode(to: encoder)
-    case .circle(let data):
-      try container.encode("circle", forKey: .type)
-      try data.encode(to: encoder)
     }
   }
 
