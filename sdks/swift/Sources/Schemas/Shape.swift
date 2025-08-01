3cafdf92fb9a1937da55711d15c508b75c47f38f
Anar Kafkas <anarkafkas@gmail.com>
2025-08-01 14:12:27 +0300

    Generate

--- sdks/swift/Sources/Schemas/Shape.swift
+++ sdks/swift/Sources/Schemas/Shape.swift
@@ -9,17 +9,99 @@ public enum Shape: Codable, Hashable, Sendable {
         public let b: Double
         public let c: Double
         public let additionalProperties: [String: JSONValue]
+
+        public init(
+            a: Double,
+            b: Double,
+            c: Double,
+            additionalProperties: [String: JSONValue] = .init()
+        ) {
+            self.a = a
+            self.b = b
+            self.c = c
+            self.additionalProperties = additionalProperties
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.a = try container.decode(Double.self, forKey: .a)
+            self.b = try container.decode(Double.self, forKey: .b)
+            self.c = try container.decode(Double.self, forKey: .c)
+            self.additionalProperties = try decoder.decodeAdditionalProperties(using: CodingKeys.self)
+        }
+
+        public func encode(to encoder: Encoder) throws -> Void {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try encoder.encodeAdditionalProperties(self.additionalProperties)
+            try container.encode(self.a, forKey: .a)
+            try container.encode(self.b, forKey: .b)
+            try container.encode(self.c, forKey: .c)
+        }
+
+        enum CodingKeys: String, CodingKey, CaseIterable {
+            case a
+            case b
+            case c
+        }
     }
 
     public struct Square: Codable, Hashable, Sendable {
         public let type: String = "square"
         public let length: Double
         public let additionalProperties: [String: JSONValue]
+
+        public init(
+            length: Double,
+            additionalProperties: [String: JSONValue] = .init()
+        ) {
+            self.length = length
+            self.additionalProperties = additionalProperties
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.length = try container.decode(Double.self, forKey: .length)
+            self.additionalProperties = try decoder.decodeAdditionalProperties(using: CodingKeys.self)
+        }
+
+        public func encode(to encoder: Encoder) throws -> Void {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try encoder.encodeAdditionalProperties(self.additionalProperties)
+            try container.encode(self.length, forKey: .length)
+        }
+
+        enum CodingKeys: String, CodingKey, CaseIterable {
+            case length
+        }
     }
 
     public struct Circle: Codable, Hashable, Sendable {
         public let type: String = "circle"
         public let radius: Double
         public let additionalProperties: [String: JSONValue]
+
+        public init(
+            radius: Double,
+            additionalProperties: [String: JSONValue] = .init()
+        ) {
+            self.radius = radius
+            self.additionalProperties = additionalProperties
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.radius = try container.decode(Double.self, forKey: .radius)
+            self.additionalProperties = try decoder.decodeAdditionalProperties(using: CodingKeys.self)
+        }
+
+        public func encode(to encoder: Encoder) throws -> Void {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try encoder.encodeAdditionalProperties(self.additionalProperties)
+            try container.encode(self.radius, forKey: .radius)
+        }
+
+        enum CodingKeys: String, CodingKey, CaseIterable {
+            case radius
+        }
     }
 }
