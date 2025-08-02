861d7a9e83525929fdc91b30c01366b7dbaad92a
Anar Kafkas <anarkafkas@gmail.com>
2025-08-03 02:32:39 +0300

    Regenerate

---
+++ sdks/swift/Sources/Schemas/DocumentPath.swift
@@ -0,0 +1,33 @@
+public enum DocumentPath: Codable, Hashable, Sendable {
+    case string(String)
+    case stringArray([String])
+    case int(Int)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let value = try? container.decode(String.self) {
+            self = .string(value)
+        } else if let value = try? container.decode([String].self) {
+            self = .stringArray(value)
+        } else if let value = try? container.decode(Int.self) {
+            self = .int(value)
+        } else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Unexpected value."
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .stringArray(let value):
+            try container.encode(value)
+        case .int(let value):
+            try container.encode(value)
+        }
+    }
+}
