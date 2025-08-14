710e385c2127a55174645d0d4e23c1beed471b45
Anar Kafkas <anarkafkas@gmail.com>
2025-08-14 14:34:07 +0300

    TS client

--- ts-client/client.ts
+++ ts-client/client.ts
@@ -1,1 +1,12 @@
-import { SeedApi } from "../sdks/typescript/src";
+import { SeedApiClient } from "../sdks/typescript/src";
+
+const client = new SeedApiClient({
+  environment: "http://localhost:3000",
+});
+
+async function main() {
+  const binaryResponse = await client.service.downloadFile();
+  console.log(binaryResponse);
+}
+
+void main();
