1124a53ee2a7632e7863a2d6f1437ac18394cdc8
Anar Kafkas <anarkafkas@gmail.com>
2025-08-14 14:27:58 +0300

    Convert to fileDownload

--- sdks/swift/Sources/Resources/Service/ServiceClient.swift
+++ sdks/swift/Sources/Resources/Service/ServiceClient.swift
@@ -7,10 +7,18 @@ public final class ServiceClient: Sendable {
         self.httpClient = HTTPClient(config: config)
     }
 
-    public func download(id: String, requestOptions: RequestOptions? = nil) async throws -> JSONValue {
+    public func simple(requestOptions: RequestOptions? = nil) async throws -> Void {
         return try await httpClient.performRequest(
-            method: .get,
-            path: "/download-content/\(id)",
+            method: .post,
+            path: "/snippet",
+            requestOptions: requestOptions
+        )
+    }
+
+    public func downloadFile(requestOptions: RequestOptions? = nil) async throws -> JSONValue {
+        return try await httpClient.performRequest(
+            method: .post,
+            path: "/",
             requestOptions: requestOptions,
             responseType: JSONValue.self
         )
