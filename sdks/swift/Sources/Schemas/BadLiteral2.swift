bef42c58ee8ead20589f916e752ca9f3e645c82d
Anar Kafkas <anarkafkas@gmail.com>
2025-08-19 09:32:18 +0300

    Fixes

--- sdks/swift/Sources/Schemas/BadLiteral2.swift
+++ sdks/swift/Sources/Schemas/BadLiteral2.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public enum BadLiteral2: String, Codable, Hashable, CaseIterable, Sendable {
-    case value1 = "1"
+    case value = "1"
 }
--- sdks/swift/Sources/Schemas/BadLiteral3.swift
+++ sdks/swift/Sources/Schemas/BadLiteral3.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public enum BadLiteral3: String, Codable, Hashable, CaseIterable, Sendable {
-    case value314 = "3.14"
+    case value = "3.14"
 }
