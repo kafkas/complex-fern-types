7a2f1fe1766ed74180c46b1a8ab29893f7e69c97
Anar Kafkas <anarkafkas@gmail.com>
2025-09-11 20:30:51 +0300

    Regenerate swift

--- sdks/swift/Sources/Schemas/Extensive.swift
+++ sdks/swift/Sources/Schemas/Extensive.swift
@@ -4,6 +4,9 @@ public struct Extensive: Codable, Hashable, Sendable {
     public let name: String
     public let creationDate: CalendarDate
     public let creationTime: Date
+    public let nullableString: JSONValue
+    public let nullableOptionalInt: JSONValue
+    public let optionalNullableString: JSONValue?
     /// Additional properties that are not explicitly defined in the schema
     public let additionalProperties: [String: JSONValue]
 
@@ -11,11 +14,17 @@ public struct Extensive: Codable, Hashable, Sendable {
         name: String,
         creationDate: CalendarDate,
         creationTime: Date,
+        nullableString: JSONValue,
+        nullableOptionalInt: JSONValue,
+        optionalNullableString: JSONValue? = nil,
         additionalProperties: [String: JSONValue] = .init()
     ) {
         self.name = name
         self.creationDate = creationDate
         self.creationTime = creationTime
+        self.nullableString = nullableString
+        self.nullableOptionalInt = nullableOptionalInt
+        self.optionalNullableString = optionalNullableString
         self.additionalProperties = additionalProperties
     }
 
@@ -24,6 +33,9 @@ public struct Extensive: Codable, Hashable, Sendable {
         self.name = try container.decode(String.self, forKey: .name)
         self.creationDate = try container.decode(CalendarDate.self, forKey: .creationDate)
         self.creationTime = try container.decode(Date.self, forKey: .creationTime)
+        self.nullableString = try container.decode(JSONValue.self, forKey: .nullableString)
+        self.nullableOptionalInt = try container.decode(JSONValue.self, forKey: .nullableOptionalInt)
+        self.optionalNullableString = try container.decodeIfPresent(JSONValue.self, forKey: .optionalNullableString)
         self.additionalProperties = try decoder.decodeAdditionalProperties(using: CodingKeys.self)
     }
 
@@ -33,6 +45,9 @@ public struct Extensive: Codable, Hashable, Sendable {
         try container.encode(self.name, forKey: .name)
         try container.encode(self.creationDate, forKey: .creationDate)
         try container.encode(self.creationTime, forKey: .creationTime)
+        try container.encode(self.nullableString, forKey: .nullableString)
+        try container.encode(self.nullableOptionalInt, forKey: .nullableOptionalInt)
+        try container.encodeIfPresent(self.optionalNullableString, forKey: .optionalNullableString)
     }
 
     /// Keys for encoding/decoding struct properties.
@@ -40,5 +55,8 @@ public struct Extensive: Codable, Hashable, Sendable {
         case name
         case creationDate = "creation_date"
         case creationTime = "creation_time"
+        case nullableString = "nullable_string"
+        case nullableOptionalInt = "nullable_optional_int"
+        case optionalNullableString = "optional_nullable_string"
     }
 }
