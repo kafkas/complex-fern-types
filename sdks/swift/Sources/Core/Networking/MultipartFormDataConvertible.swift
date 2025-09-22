99dcbf1155eedafc6a57293663a07983f3ff2918
Anar Kafkas <anarkafkas@gmail.com>
2025-09-22 15:14:09 +0300

    More cleanup

--- sdks/swift/Sources/Core/Networking/MultipartFormDataConvertible.swift
+++ sdks/swift/Sources/Core/Networking/MultipartFormDataConvertible.swift
@@ -6,32 +6,6 @@ protocol MultipartFormDataConvertible {
     var multipartFields: [MultipartField] { get }
 }
 
-/// Type-erased wrapper for encodable values
-struct EncodableValue {
-    private let value: any Encodable
-    
-    init<T: Encodable>(_ value: T) {
-        self.value = value
-    }
-    
-    func jsonEncode(using encoder: JSONEncoder) throws -> Data {
-        return try encoder.encode(AnyEncodable(value))
-    }
-}
-
-/// Helper for type-erasing Encodable values
-private struct AnyEncodable: Encodable {
-    private let value: any Encodable
-    
-    init(_ value: any Encodable) {
-        self.value = value
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        try value.encode(to: encoder)
-    }
-}
-
 /// Represents a field in multipart form data
 enum MultipartField {
     /// A single file field
@@ -40,11 +14,7 @@ enum MultipartField {
     case fileArray(_ files: [FormFile], fieldName: String)
     /// A text field with JSON-encoded value (for strings, numbers, booleans, dates, etc.)
     case field(_ value: EncodableValue, fieldName: String)
-}
 
-// MARK: - Convenience methods for common types
-
-extension MultipartField {
     /// Create a text field from any Encodable value
     static func field<T: Encodable>(_ value: T, fieldName: String) -> MultipartField {
         return .field(EncodableValue(value), fieldName: fieldName)
@@ -64,14 +34,19 @@ extension MultipartFormDataConvertible {
             case .fileArray(let files, let fieldName):
                 for file in files {
                     multipartData.appendFile(
-                        file.data, withName: fieldName, fileName: file.filename)
+                        file.data,
+                        withName: fieldName,
+                        fileName: file.filename
+                    )
                 }
             case .field(let encodableValue, let fieldName):
                 do {
-                    let encodedData = try encodableValue.jsonEncode(using: jsonEncoder)
+                    let encodedData = try jsonEncoder.encode(value: encodableValue)
                     if let encodedString = String(data: encodedData, encoding: .utf8) {
                         // Remove quotes from simple JSON values (strings, numbers, booleans)
-                        let finalValue = encodedString.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+                        let finalValue: String = encodedString.trimmingCharacters(
+                            in: CharacterSet(charactersIn: "\"")
+                        )
                         multipartData.appendField(finalValue, withName: fieldName)
                     }
                 } catch {
